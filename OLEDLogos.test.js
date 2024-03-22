590ed7879c9ee51f8aec66199414452a7979dc06
Rick van Modem <rick@vanmodem.nl>
2024-03-22 19:23:05 +0100

    Added a way of animating the screen. Added a Kirby animation. Also updated the SignalRGB logo

---
+++ OLEDLogos.test.js
@@ -0,0 +1,9 @@
+import kirby from './Screens/kirby.test.js';
+import signalRGB from './Screens/signalrgb.test.js';
+
+const OLEDLogos = {
+    Kirby: kirby,
+    SignalRGB: signalRGB
+}
+
+export default OLEDLogos;
