e0feefc8a6ad652dabde6ca36287ceb02e13ee2f
Rick van Modem <rick@vanmodem.nl>
2024-03-24 23:35:12 +0100

    Added Geometric Triangles

--- OLEDLogos.test.js
+++ OLEDLogos.test.js
@@ -1,9 +1,11 @@
 import signalRGB from './Screens/signalrgb.test.js';
+import geotri from './Screens/geometric-triangle.test.js';
 import kirby from './Screens/kirby.test.js';
 import skull from './Screens/skull.test.js';
 
 const OLEDLogos = {
     Default: signalRGB,
+    'Geometric Triangles (animation)': geotri,
     "Kirby (animation)": kirby,
     "Skull (animation)": skull,
 }
