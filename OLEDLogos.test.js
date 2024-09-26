6a74f5a78741831051bf93225305da8464ef84de
RICK-OFFICIAL\rick <rick@rick.games>
2024-09-26 18:56:06 +0200

    Added cat animation

--- OLEDLogos.test.js
+++ OLEDLogos.test.js
@@ -2,12 +2,14 @@ import signalRGB from './Screens/signalrgb.test.js';
 import geotri from './Screens/geometric-triangle.test.js';
 import kirby from './Screens/kirby.test.js';
 import skull from './Screens/skull.test.js';
+import cat from './Screens/cat.test.js';
 
 const OLEDLogos = {
     Default: signalRGB,
     'Geometric Triangles (animation)': geotri,
     "Kirby (animation)": kirby,
     "Skull (animation)": skull,
+    "Cat (animation)": cat,
 }
 
 export default OLEDLogos;
